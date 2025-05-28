8ca7d8bc2403b8e8cc85687fcc1040d461f87215
aurélien.adnot <a.adnot@gmail.com>
2025-05-28 10:36:36 +0200

    Create machine from createMachine

---
+++ src/machine/machine.ts
@@ -0,0 +1,51 @@
+import {createMachine} from "xstate"
+
+enum GameStates {
+  LOBBY = "LOBBY",
+  PLAY = "PLAY",
+  VICTORY = "VICTORY",
+  DRAW = "DRAW"
+}
+export const machine = createMachine({
+  id: "game",
+  initial: GameStates.LOBBY,
+  states:{
+    [GameStates.LOBBY]:{
+      on: {
+        join:{
+          target: GameStates.LOBBY
+        },
+        leave:{
+          target: GameStates.LOBBY
+        },
+        chooseColor:{
+          target: GameStates.LOBBY
+        },
+        start:{
+          target: GameStates.PLAY
+        }
+      }
+    },
+    [GameStates.PLAY]:{
+      on:{
+        dropToken:{
+          target: "???"
+        }
+      }
+    },
+    [GameStates.DRAW]:{
+      on:{
+        restart:{
+          target: GameStates.LOBBY
+        }
+      }
+    },
+    [GameStates.VICTORY]:{
+      on:{
+        restart:{
+          target: GameStates.LOBBY
+        }
+      }
+    }
+  }
+})

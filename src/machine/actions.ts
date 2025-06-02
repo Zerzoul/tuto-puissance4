74d5db67f05dbdad8a6c73437b99f5cfa4a81c29
aurélien.adnot <a.adnot@gmail.com>
2025-06-02 11:37:54 +0200

    refacto: dropToken add action at the end create: saveWiningPositionAction to save the position on gameModel

--- src/machine/actions.ts
+++ src/machine/actions.ts
@@ -1,4 +1,4 @@
-import { freePositionY } from "../func/game";
+import { currentPlayer, freePositionY, winingPositions } from "../func/game";
 import type { GameAction, GameContext } from "../types";
 
 export const joinGameAction: GameAction<"join"> = (context, event) => ({
@@ -13,7 +13,7 @@ export const leaveGameAction: GameAction<"join"> = (context, event) => ({
 //   players: [...context.players, {id: event.playerId, color: event.color}]
 // })
 
-export const dropToken: GameAction<"dropToken"> = ({grid, players}, {x: eventX, playerId}) => {
+export const dropTokenAction: GameAction<"dropToken"> = ({grid, players}, {x: eventX, playerId}) => {
   const playerColor = players.find(p=>p.id === playerId)!.color!
   const eventY = freePositionY(grid, eventX)
   const newGrid = grid.map((row, y) => row.map((v, x) => x === eventX && y === eventY ? playerColor : v))
@@ -24,4 +24,13 @@ export const dropToken: GameAction<"dropToken"> = ({grid, players}, {x: eventX, 
 
 export const switchPlayerAction = (context: GameContext) => ({
   currentPlayer: context.players.find(p=>p.id !== context.currentPlayer)!.id
+})
+
+export const saveWiningPositionsAction: GameAction<"dropToken"> = (context, event) => ({
+  winingPositions: winingPositions(
+      context.grid, 
+      currentPlayer(context).color!,
+      event.x,
+      context.rowLength
+    )
 })
